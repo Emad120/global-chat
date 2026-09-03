25ad5eaad38402bb63311056bd2451e79e67f008
Emad120 <emadhlaweh@gmail.com>
2026-09-03 19:17:25 +0300

    Add Supabase configuration to config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://eqlfuyvndvpmmzfthacr.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_0RSRXxjdE-w81kXwYUL7kQ_rPSIIl75';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
